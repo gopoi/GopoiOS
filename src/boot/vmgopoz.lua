6c308697d1c2a344aea800ab7b836be042a85fc0
Shan Meunier Bernard <tchoutchawn@gmail.com>
2015-04-25 23:53:34 -0400

    Added steps for init and kernel

---
+++ src/boot/vmgopoz.lua
@@ -0,0 +1,55 @@
+--[[ NON-POSIG
+  gopoiOS kernel
+  Author: Shan B.
+  Date: 25/04/2015 
+]]--
+
+-- Startup shits
+
+
+
+-- Basic kernel functions
+
+
+
+-- Parse arguments
+
+
+
+-- Create virtual filesystem
+
+
+
+-- Attach Fs driver
+
+
+
+-- Mount rootfs on 
+
+
+
+-- Load and attach required kernel modules
+
+
+
+-- Mount system filesystems
+
+
+
+-- Sandbox everything
+
+
+
+-- execute rc file
+
+
+
+-- Start scheduling
+
+
+
+-- Scheduling and shits
+
+
+
+-- Shutdown 
---
+++ src/init.lua
@@ -0,0 +1,20 @@
+--[[ NON-POSIG
+  gopoiOS kernel bootstrapper for OpenComputers
+  Author: Shan B.
+  Date: 25/04/2015 
+]]--
+
+-- Map base components for the bootstrapping process
+
+---- Locate and map Gpu and screen
+
+---- Locate bootable Filesystem
+
+---- Locate Kernel in bootable filesystem
+
+---- Locate and load kernel Fs driver 
+
+---- Load kernel
+
+---- Boot kernel
+
